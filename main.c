0c813f82835ae8d96ac845b00c5477b6202436c7
Daleth <i.publish.docs@gmail.com>
2015-10-16 23:28:48 -0700

    Added arbitrary tones

--- main.c
+++ main.c
@@ -15,6 +15,9 @@
 #define BOOLEAN__     UINT8
 #define TRUE__        1
 #define FALSE__       0
+
+    // Short macro functions for inlining common expressions
+#define IS_NULL(P) (P == NULL)
     /**********   End type aliasing   **********/
 
     /**********   Start function prototypes   **********/
@@ -563,6 +566,9 @@ static UINT16 sin_samps[SAMP_POP] = {
 	1007,  1007,  1007,  1007,  1007,  1007,  1007,  1007,  1007,  1007, 
 	1007,  1007,  1007,  1008,  1008,  1008,  1008,  1008,  1008,  1008
 	};
+
+static const UINT16 arb_tones[9] = {3, 6, 9, 12, 15, 18, 21, 24, 27};
+
     /**********   End global variables   **********/
 
 int main (void)
@@ -668,6 +674,12 @@ void run_tone_generator(void){
             samp_arr[3] = (sample_speed/1000u)%10u;
         } else if(row == 0x0 && col == 0x8 && (sample_speed > 0)){
             --sample_speed;
+            samp_arr[0] = sample_speed%10u;
+            samp_arr[1] = (sample_speed/10u)%10u;
+            samp_arr[2] = (sample_speed/100u)%10u;
+            samp_arr[3] = (sample_speed/1000u)%10u;
+        } else if(row > 0x0 && col > 0x0){
+            sample_speed = arb_tones[(3-row+1)*3 - find_lsob(col)];
             samp_arr[0] = sample_speed%10u;
             samp_arr[1] = (sample_speed/10u)%10u;
             samp_arr[2] = (sample_speed/100u)%10u;
@@ -846,7 +858,7 @@ void check_key(UINT8* row_dest, UINT8* col_dest){
     porta->OUT.reg &= ~(1u << (4u + cur_row));
         // Extract the four bits we're interested in from
         //   the keypad.
-    if(!IS_NULL(col_dest))  *col_dest = debounce_keypress();
+    if(!IS_NULL(col_dest))  *col_dest = (porta->IN.reg >> 16u) & 0xF;
     if(!IS_NULL(row_dest))  *row_dest = cur_row;
 
         // Prepare for the next row. If we were on the last
@@ -855,52 +867,6 @@ void check_key(UINT8* row_dest, UINT8* col_dest){
         //  we were not on the last row. If that is the case,
         //  reset row_bit to 0.
     cur_row = (cur_row+1u)%4u;
-}
-
-UINT8 debounce_keypress(void){
-    // Triggered the instant the first key press is detected
-    //  Returns the resulting hex number
-
-    UINT8 toreturn = (porta->IN.reg >> 16u) & 0xF;
-
-        // Check if more than one button in a row was pressed.
-        //  If so, checking for glitches is no longer important.
-    UINT32 counter = 0x0;
-    BOOLEAN__ already_on = FALSE__;
-    for(; counter < 4; ++counter){
-        if(already_on & (toreturn >> counter))  return toreturn;
-        else    already_on = (toreturn >> counter) & 0x1;
-    }
-
-    if(!toreturn)   return 0x0;
-#define MAX_JITTER  5
-#define MAX_JITTER2 1000
-#define RELEASE_LIM 7500
-
-    // First, read up to MAX_JITTER times to swallow spikes as button is
-    //  pressed. If no key press was detected in this time, the noise is
-    //  not from a button press.
-    for(counter = 0x0; counter < MAX_JITTER; ++counter){
-        if(!((porta->IN.reg >> 16u) & 0xF))    return 0x0;
-    }
-
-    // Now swallow the spikes as the button is released. Do not exit
-    //  until the spikes are no longer detected after MAX_JITTER reads.
-    //  If the user is holding down the button, release manually based
-    //  on RELEASE_LIM.
-    volatile UINT32 release = 0x0;
-    for(
-        counter = 0x0;
-        counter < MAX_JITTER2 && release < RELEASE_LIM;
-        ++counter, ++release
-    ){
-        if((porta->IN.reg >> 16u) & 0xF)    counter = 0x0;
-    }
-
-    return toreturn;
-
-#undef MAX_JITTER
-#undef RELEASE_LIM
 }
 
 UINT32 find_lsob(UINT32 target){
